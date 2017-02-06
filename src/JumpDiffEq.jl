05f2dfc5c1b49e74d35fe4c21c42bfcda70970ba
ChrisRackauckas <me@chrisrackauckas.com>
2017-02-05 17:59:20 -0800

    add Gillespie problem

--- src/JumpDiffEq.jl
+++ src/JumpDiffEq.jl
@@ -8,12 +8,17 @@ abstract AbstractJump
 abstract AbstractJumpProblem{P,J} <: DEProblem
 
 include("jumps.jl")
+include("gillespie.jl")
 include("problem.jl")
 include("callbacks.jl")
 include("compound_constant.jl")
 include("solve.jl")
 
-export ConstantRateJump, VariableRateJump, JumpSet, JumpProblem, CompoundConstantRateJump
+export ConstantRateJump, VariableRateJump, JumpSet, CompoundConstantRateJump
+
+export JumpProblem, GillespieProblem
+
+export Reaction, ReactionSet, build_jumps_from_reaction
 
 export init, solve, solve!
 
--- src/problem.jl
+++ src/problem.jl
@@ -7,3 +7,6 @@ JumpProblem(prob,jumps::ConstantRateJump) = JumpProblem(prob,JumpSet(jumps))
 JumpProblem(prob,jumps::VariableRateJump) = JumpProblem(prob,JumpSet(jumps))
 JumpProblem(prob,jumps::JumpSet) = JumpProblem(prob,jumps)
 JumpProblem(prob,jumps...) = JumpProblem(prob,JumpSet(jumps...))
+
+GillespieProblem(prob,rs::Reaction...) = JumpProblem(prob,
+                                         build_jumps_from_reaction(rs...))

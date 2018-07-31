68a50289c1fd46ff0eb9769d9c12c0690efb2002
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-07-31 06:24:38 -0700

    fix ExtendedJumpArray

--- src/extended_jump_array.jl
+++ src/extended_jump_array.jl
@@ -23,6 +23,8 @@ end
 Base.IndexStyle(::Type{<:ExtendedJumpArray}) = IndexLinear()
 Base.similar(A::ExtendedJumpArray) = ExtendedJumpArray(similar(A.u),similar(A.jump_u))
 Base.similar(A::ExtendedJumpArray,::Type{S}) where {S} = ExtendedJumpArray(similar(A.u,S),similar(A.jump_u,S))
+# Ignore axes
+Base.similar(A::ExtendedJumpArray,::Type{S},axes::Tuple{Base.OneTo{Int}}) where {S} = ExtendedJumpArray(similar(A.u,S),similar(A.jump_u,S))
 
 function recursivecopy!(dest::T, src::T) where T<:ExtendedJumpArray
   recursivecopy!(dest.u,src.u)
@@ -33,78 +35,47 @@ Base.show(io::IO,A::ExtendedJumpArray) = show(io,A.u)
 TreeViews.hastreeview(x::ExtendedJumpArray) = true
 plot_indices(A::ExtendedJumpArray) = eachindex(A.u)
 
-add_idxs1(x,expr) = expr
-add_idxs1(::Type{T},expr) where {T<:ExtendedJumpArray} = :($(expr).u)
+###### Broadcast overloading
 
-add_idxs2(x,expr) = expr
-add_idxs2(::Type{T},expr) where {T<:ExtendedJumpArray} = :($(expr).jump_u)
+const ExtendedJumpArrayStyle = Broadcast.ArrayStyle{ExtendedJumpArray}
+Base.BroadcastStyle(::Type{<:ExtendedJumpArray}) = Broadcast.ArrayStyle{ExtendedJumpArray}()
+Base.BroadcastStyle(::Broadcast.ArrayStyle{ExtendedJumpArray},::Broadcast.ArrayStyle) = Broadcast.ArrayStyle{ExtendedJumpArray}()
+Base.BroadcastStyle(::Broadcast.ArrayStyle,::Broadcast.ArrayStyle{ExtendedJumpArray}) = Broadcast.ArrayStyle{ExtendedJumpArray}()
+Base.similar(bc::Broadcast.Broadcasted{Broadcast.ArrayStyle{ExtendedJumpArray}},::Type{ElType}) where ElType = similar(bc)
 
-@generated function Base.broadcast!(f,A::ExtendedJumpArray,B::Union{Number,ExtendedJumpArray}...)
-  exs1 = ((add_idxs1(B[i],:(B[$i])) for i in eachindex(B))...,)
-  exs2 = ((add_idxs2(B[i],:(B[$i])) for i in eachindex(B))...,)
+add_idxs1(A,x,expr) = expr
+add_idxs1(A,::Type{T},expr) where {T<:ExtendedJumpArray} = :($(expr).u)
+add_idxs1(A,::Type{T},expr) where {T<:AbstractArray} = :(@view($(expr)[1:length(A.u)]))
+
+add_idxs2(A,x,expr) = expr
+add_idxs2(A,::Type{T},expr) where {T<:ExtendedJumpArray} = :($(expr).jump_u)
+add_idxs2(A,::Type{T},expr) where {T<:AbstractArray} = :(@view($(expr)[1:length(A.jump_u)]))
+
+function Base.copy(bc::Broadcast.Broadcasted{ExtendedJumpArrayStyle})
+    ret = Broadcast.flatten(bc)
+    __broadcast(ret.f,ret.args...)
+end
+
+function Base.copyto!(dest::AbstractArray, bc::Broadcast.Broadcasted{ExtendedJumpArrayStyle})
+    ret = Broadcast.flatten(bc)
+    __broadcast!(ret.f,dest,ret.args...)
+end
+
+@generated function __broadcast(f,A::ExtendedJumpArray,B...)
+  exs1 = ((add_idxs1(A,B[i],:(B[$i])) for i in eachindex(B))...,)
+  exs2 = ((add_idxs2(A,B[i],:(B[$i])) for i in eachindex(B))...,)
+  res = quote
+      ExtendedJumpArray(broadcast(f,A.u,$(exs1...)),broadcast(f,A.jump_u,$(exs2...)))
+  end
+  res
+end
+
+@generated function __broadcast!(f,A::ExtendedJumpArray,B...)
+  exs1 = ((add_idxs1(A,B[i],:(B[$i])) for i in eachindex(B))...,)
+  exs2 = ((add_idxs2(A,B[i],:(B[$i])) for i in eachindex(B))...,)
   res = quote
       broadcast!(f,A.u,$(exs1...));broadcast!(f,A.jump_u,$(exs2...))
-    end
-  res
-end
-
-Base.Broadcast.promote_containertype(::Type{T}, ::Type{T}) where {T<:ExtendedJumpArray} = T
-Base.Broadcast.promote_containertype(::Type{T}, ::Type{S}) where {T<:ExtendedJumpArray, S<:AbstractArray} = T
-Base.Broadcast.promote_containertype(::Type{S}, ::Type{T}) where {T<:ExtendedJumpArray, S<:AbstractArray} = T
-Base.Broadcast.promote_containertype(::Type{T}, ::Type{<:Any}) where {T<:ExtendedJumpArray} = T
-Base.Broadcast.promote_containertype(::Type{<:Any}, ::Type{T}) where {T<:ExtendedJumpArray} = T
-Base.Broadcast.promote_containertype(::Type{Array}, ::Type{T}) where {T<:ExtendedJumpArray} = T
-Base.Broadcast.promote_containertype(::Type{T}, ::Type{Array}) where {T<:ExtendedJumpArray} = T
-Base.Broadcast._containertype(::Type{T}) where {T<:ExtendedJumpArray} = T
-Base.Broadcast.broadcast_indices(::Type{<:ExtendedJumpArray}, A) = indices(A)
-
-@inline function Base.Broadcast.broadcast_c(f, ::Type{S}, A, Bs...) where S<:ExtendedJumpArray
-    T = Base.Broadcast._broadcast_eltype(f, A, Bs...)
-    shape = Base.Broadcast.broadcast_indices(A, Bs...)
-    broadcast!(f, similar(A), A, Bs...)
-end
-
-@inline function Base.Broadcast.broadcast_c(f, ::Type{S}, A::ExtendedJumpArray, Bs::Union{ExtendedJumpArray,Number}...) where S<:ExtendedJumpArray
-    new_A = similar(A)
-    broadcast!(f,new_A,A,Bs...)
-    new_A
-end
-
-@inline function Base.Broadcast.broadcast_c(f, ::Type{S}, A::ExtendedJumpArray) where S<:ExtendedJumpArray
-    new_A = similar(A)
-    broadcast!(f,new_A,A)
-    new_A
-end
-
-@inline Base.broadcast!(::typeof(identity), u::DiffEqJump.ExtendedJumpArray, x::Number) = fill!(u,x)
-#=
-Base.Broadcast._containertype(::Type{<:ExtendedJumpArray}) = ExtendedJumpArray
-Base.Broadcast.promote_containertype(::Type{ExtendedJumpArray}, _) = ExtendedJumpArray
-Base.Broadcast.promote_containertype(_, ::Type{ExtendedJumpArray}) = ExtendedJumpArray
-Base.Broadcast.promote_containertype(::Type{ExtendedJumpArray}, ::Type{Array}) = ExtendedJumpArray
-Base.Broadcast.promote_containertype(::Type{Array}, ::Type{ExtendedJumpArray}) = ExtendedJumpArray
-
-@generated function Base.broadcast_c(f,::Type{ExtendedJumpArray},B...)
-  exs1 = ((add_idxs1(B[i],:(B[$i])) for i in eachindex(B))...)
-  exs2 = ((add_idxs2(B[i],:(B[$i])) for i in eachindex(B))...)
-  res = quote
-          @show B
-          for b in B
-            @show b
-            @show typeof(b)
-            @show typeof(b) <: ExtendedJumpArray
-            if typeof(b) <: ExtendedJumpArray
-              @show "here"
-              L = length(b.u)
-              @show L
-              break
-            end
-          end
-          @show L
-          ExtendedJumpArray(broadcast(f,$(exs1...)),broadcast(f,$(exs2...)))
-        end
+      A
   end
-  @show res
   res
 end
-=#
--- test/variable_rate.jl
+++ test/variable_rate.jl
@@ -5,11 +5,20 @@ b = ExtendedJumpArray(rand(3),rand(2))
 
 a.=b
 
+@test a.u == b.u
+@test a.jump_u == b.jump_u
 @test a == b
+
+c = rand(5)
+d = 2.0
+
+a .+ d
+a .= b .+ d
+a .+ c .+ d
+a .= b .+ c .+ d
 
 rate = (u,p,t) -> u[1]
 affect! = (integrator) -> (integrator.u[1] = integrator.u[1]/2)
-
 jump = VariableRateJump(rate,affect!,interp_points=1000)
 jump2 = deepcopy(jump)
 
@@ -22,7 +31,7 @@ jump_prob = JumpProblem(prob,Direct(),jump,jump2)
 
 integrator = init(jump_prob,Tsit5(),dt=1/10)
 
-sol = solve(jump_prob,Tsit5())
+sol = solve(jump_prob,Tsit5(),dt=1/10)
 
 @test maximum([sol[i][2] for i in 1:length(sol)]) <= 1e-12
 @test maximum([sol[i][3] for i in 1:length(sol)]) <= 1e-12

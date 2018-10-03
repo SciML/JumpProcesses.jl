d8250e9172101c3600bcc3ad10fa03aa6ac6031c
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-10-03 14:55:14 +0100

    fix rng seeding for monte carlo

--- src/solve.jl
+++ src/solve.jl
@@ -11,9 +11,12 @@ end
 function DiffEqBase.__init(
   jump_prob::DiffEqBase.AbstractJumpProblem{P},
   alg::DiffEqBase.DEAlgorithm,timeseries=[],ts=[],ks=[],recompile::Type{Val{recompile_flag}}=Val{true};
-  callback=nothing,
+  callback=nothing, seed = rand(UInt64),
   kwargs...) where {P,recompile_flag}
 
+  if !isempty(jump_prob.jump_callback.discrete_callbacks)
+    Random.seed!(jump_prob.jump_callback.discrete_callbacks[1].condition.rng,seed)
+  end
   integrator = init(jump_prob.prob,alg,timeseries,ts,ks,recompile;
                     callback=CallbackSet(callback,jump_prob.jump_callback),
                     kwargs...)

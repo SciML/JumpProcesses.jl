fe949d4b78f600e482c211f9561e5c52c506b7a4
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-04-20 18:06:23 -0400

    fix all tests... and add a new one.

---
+++ test/ensemble_uniqueness.jl
@@ -0,0 +1,15 @@
+using OrdinaryDiffEq, DiffEqJump, DiffEqBiological, Test
+j1 = ConstantRateJump((u,p,t)->10,(integrator)->integrator.u[1]+=1)
+j2 = ConstantRateJump((u,p,t)->1u[1],(integrator)->integrator.u[1]-=1)
+u0 = [0]
+
+prob = DiscreteProblem(u0, (0., 100.), [])
+jump_prob = JumpProblem(prob, Direct(), j1, j2)
+sol = solve(EnsembleProblem(jump_prob), FunctionMap(), trajectories=3)
+@test Array(sol[1]) !== Array(sol[2])
+@test Array(sol[1]) !== Array(sol[3])
+@test Array(sol[2]) !== Array(sol[3])
+sol = solve(EnsembleProblem(jump_prob), SSAStepper(), trajectories=3)
+@test Array(sol[1]) !== Array(sol[2])
+@test Array(sol[1]) !== Array(sol[3])
+@test Array(sol[2]) !== Array(sol[3])

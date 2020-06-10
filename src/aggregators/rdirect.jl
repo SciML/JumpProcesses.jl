643dac5cd764acbd82cc48d95a505c8e875613ea
Vilin97 <vasilin97@gmail.com>
2020-06-10 19:57:34 -0400

    bug

--- src/aggregators/rdirect.jl
+++ src/aggregators/rdirect.jl
@@ -122,7 +122,7 @@ function update_dependent_rates!(p::RDirectJumpAggregation, u, params, t)
         sum_rate += new_rate
         if new_rate > p.max_rate
             p.max_rate = new_rate
-        else if abs(cur_rates[rx]-p.max_rate) < eps(typeof(p.max_rate))
+        elseif abs(cur_rates[rx]-p.max_rate) < eps(typeof(p.max_rate))
             need_to_reculculate_max_rate = false
         end
         cur_rates[rx] = new_rate

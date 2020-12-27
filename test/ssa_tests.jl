69a332a042f3285eeafea34dd92f0106abe28efc
Manuel Reinhardt <manuel.rhdt@gmail.com>
2020-12-27 18:32:45 +0100

    Add tests for save_start and save_end

--- test/ssa_tests.jl
+++ test/ssa_tests.jl
@@ -20,8 +20,23 @@ integrator = init(jump_prob,SSAStepper())
 step!(integrator)
 integrator.u[1]
 
+# test different saving behaviors
+
 sol = solve(jump_prob,SSAStepper())
+@test sol.t[begin] == 0.0
+@test sol.t[end] == 3.0
+
+sol = solve(jump_prob, SSAStepper(),save_end=false)
+@test sol.t[begin] == 0.0
+@test sol.t[end] < 3.0
+
+sol = solve(jump_prob, SSAStepper(),save_start=false)
+@test sol.t[begin] > 0.0
+@test sol.t[end] == 3.0
 
 jump_prob = JumpProblem(prob,Direct(),jump,jump2,save_positions=(false,false))
+sol = solve(jump_prob, SSAStepper(),save_start=false,save_end=false)
+@test isempty(sol.t) && isempty(sol.u)
+
 sol = solve(jump_prob,SSAStepper(),saveat=0.0:0.1:2.9)
 @test sol.t == collect(0.0:0.1:3.0)

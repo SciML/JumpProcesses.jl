1f0394c51b3b0fe15f84e085ab3b3315cd42817c
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-01-12 11:16:23 -0500

    fix jump array show

--- test/variable_rate.jl
+++ test/variable_rate.jl
@@ -35,6 +35,9 @@ sol = solve(jump_prob,Tsit5())
 sol = solve(jump_prob,Rosenbrock23(autodiff=false))
 sol = solve(jump_prob,Rosenbrock23())
 
+@show sol[end]
+display(sol[end])
+
 @test maximum([sol[i][2] for i in 1:length(sol)]) <= 1e-12
 @test maximum([sol[i][3] for i in 1:length(sol)]) <= 1e-12
 

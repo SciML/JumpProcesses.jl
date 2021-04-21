1ba13e6b51e431e9398e97d4f06d5bc1e78ae319
Samuel Isaacson <isaacsas@users.noreply.github.com>
2021-04-20 20:14:53 -0400

    import packages to test

--- test/longtimes_test.jl
+++ test/longtimes_test.jl
@@ -1,3 +1,7 @@
+using DiffEqJump, Test, StableRNGs
+
+rng = StableRNG(12345)
+
 # test for https://github.com/SciML/DiffEqJump.jl/issues/177
 p   = [1., 2., 50.]
 ns  = [[1 => 1], [1 => -1], [1 => 1]]
@@ -7,6 +11,6 @@ u0    = [5]
 tspan = (0., 2e6)
 dt    = tspan[2] / 1000
 dprob = DiscreteProblem(u0, tspan, p)
-jprob = JumpProblem(dprob, Direct(), maj, save_positions=(false,false))
+jprob = JumpProblem(dprob, Direct(), maj, save_positions=(false,false)), rng=rng
 sol   = solve(jprob, SSAStepper(), saveat=tspan[1]:dt:tspan[2])
 @test length(unique(sol[(end-10):end][:])) > 1

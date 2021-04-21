fd8f5c035dafaf1cae0b86dede1cb88a915f600f
Sam Isaacson <isaacsas@users.noreply.github.com>
2021-04-20 20:30:58 -0400

    Update longtimes_test.jl

--- test/longtimes_test.jl
+++ test/longtimes_test.jl
@@ -11,6 +11,6 @@ u0    = [5]
 tspan = (0., 2e6)
 dt    = tspan[2] / 1000
 dprob = DiscreteProblem(u0, tspan, p)
-jprob = JumpProblem(dprob, Direct(), maj, save_positions=(false,false)), rng=rng)
+jprob = JumpProblem(dprob, Direct(), maj, save_positions=(false,false), rng=rng)
 sol   = solve(jprob, SSAStepper(), saveat=tspan[1]:dt:tspan[2])
 @test length(unique(sol[(end-10):end][:])) > 1

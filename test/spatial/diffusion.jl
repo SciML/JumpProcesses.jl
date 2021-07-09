6caebf12e09363c5af267f053249a743054d0a55
Vasily Ilin <vasilin97@gmail.com>
2021-07-09 16:02:23 -0700

    something is wrong with the diffusion test

--- test/spatial/diffusion.jl
+++ test/spatial/diffusion.jl
@@ -32,9 +32,9 @@ tf = 0.5
 u0 = [100]
 
 domain_size = 1.0 #μ-meter
-linear_size = 3
+linear_size = 5
 diffusivity = 0.1
-dim = 2
+dim = 1
 grid = LightGraphs.grid([linear_size for i in 1:dim])
 num_nodes = DiffEqJump.num_sites(grid)
 
@@ -57,20 +57,21 @@ mean_sol = get_mean_sol(spatial_jump_prob, Nsims, tf/num_time_points)
 
 # analytic solution THIS IS WRONG
 lap = discrete_laplacian_from_spatial_system(grid)
+h = domain_size/linear_size
 evals, Q = eigen(lap) # lap == Q*diagm(evals)*Q'
 Qt = Q'
-analytic_solution(t) = Q*diagm(ℯ.^(diffusivity*t*evals))*Qt * reshape(prob.u0, num_nodes, 1)
+analytic_solution(t) = Q*diagm(ℯ.^(diffusivity*t*evals/h^2))*Qt * reshape(prob.u0, num_nodes, 1)
 
 # test mean solution
-# n = num_nodes
-# h = domain_size/linear_size
-# D = diffusivity
-# eval_lap(j, h, n) = -4/h^2 * (sin(pi*(j-1)/(2n)))^2
-# evec_lap(j,n) = j==1 ? [√(1/n) for i in 1:n] : [√(2/n) * cos(pi*(j-1)*(i-0.5)/n) for i in 1:n]
-# Q = hcat([evec_lap(j,n) for j in 1:n]...)
-# Qt = Q'
-# lambdas = [eval_lap(j, h, n) for j in 1:n]
-# analytic_solution(t) = Q*diagm(ℯ.^(D*t*lambdas))*Qt * reshape(prob.u0, num_nodes, 1)
+n = num_nodes
+h = domain_size/linear_size
+D = diffusivity
+eval_lap(j, h, n) = -4/h^2 * (sin(pi*(j-1)/(2n)))^2
+evec_lap(j,n) = j==1 ? [√(1/n) for i in 1:n] : [√(2/n) * cos(pi*(j-1)*(i-0.5)/n) for i in 1:n]
+Q = hcat([evec_lap(j,n) for j in 1:n]...)
+Qt = Q'
+lambdas = [eval_lap(j, h, n) for j in 1:n]
+analytic_solution(t) = Q*diagm(ℯ.^(D*t*lambdas))*Qt * reshape(prob.u0, num_nodes, 1)
 
 rel_tol = 0.01
 
@@ -78,5 +79,5 @@ times = 0.0:tf/num_time_points:tf
 for (i,t) in enumerate(times)
     local diff = analytic_solution(t) - reshape(mean_sol[i], num_nodes, 1)
     @show diff
-    # @test abs(sum(diff[1:center_node])/sum(analytic_solution(t)[1:center_node])) < rel_tol
+    @test abs(sum(diff[1:center_node])/sum(analytic_solution(t)[1:center_node])) < rel_tol
 end

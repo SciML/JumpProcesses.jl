a74f8565894310aa9c159a14b136f51863da807f
Samuel Isaacson <isaacsas@users.noreply.github.com>
2021-07-09 16:15:54 -0400

    add kwarg to disable parameter updates to jumps

--- src/aggregators/aggregated_api.jl
+++ src/aggregators/aggregated_api.jl
@@ -5,37 +5,40 @@ Reset the state of jump processes and associated solvers following a change
 in parameters or such.
 """
 
-function reset_aggregated_jumps!(integrator,uprev = nothing)
-     reset_aggregated_jumps!(integrator,uprev,integrator.opts.callback)
+function reset_aggregated_jumps!(integrator,uprev = nothing; update_jump_params=true)
+     reset_aggregated_jumps!(integrator,uprev,integrator.opts.callback,
+                             update_jump_params=update_jump_params)
      nothing
 end
 
-function reset_aggregated_jumps!(integrator,uprev,callback::Nothing)
+function reset_aggregated_jumps!(integrator,uprev,callback::Nothing; update_jump_params=true)
      nothing
 end
 
 
-function reset_aggregated_jumps!(integrator,uprev,callback::CallbackSet)
+function reset_aggregated_jumps!(integrator,uprev,callback::CallbackSet; update_jump_params=true)
     if !isempty(callback.discrete_callbacks)
-        reset_aggregated_jumps!(integrator,uprev,callback.discrete_callbacks...)
+        reset_aggregated_jumps!(integrator,uprev,callback.discrete_callbacks..., 
+                                update_jump_params=update_jump_params)
     end
     nothing
 end
 
-function reset_aggregated_jumps!(integrator,uprev,cb::DiscreteCallback,cbs...)
+function reset_aggregated_jumps!(integrator,uprev,cb::DiscreteCallback,cbs...; update_jump_params=true)
     if typeof(cb.condition) <: AbstractSSAJumpAggregator
         maj = cb.condition.ma_jumps
-        using_params(maj) && update_parameters!(cb.condition.ma_jumps,integrator.p)
+        update_jump_params && using_params(maj) && update_parameters!(cb.condition.ma_jumps,integrator.p)
         cb.condition(cb,integrator.u,integrator.t,integrator)
     end
-    reset_aggregated_jumps!(integrator,uprev,cbs...)
+    reset_aggregated_jumps!(integrator,uprev,cbs...; 
+                            update_jump_params=update_jump_params)
     nothing
 end
 
-function reset_aggregated_jumps!(integrator,uprev,cb::DiscreteCallback)
+function reset_aggregated_jumps!(integrator,uprev,cb::DiscreteCallback; update_jump_params=true)
     if typeof(cb.condition) <: AbstractSSAJumpAggregator
         maj = cb.condition.ma_jumps
-        using_params(maj) && update_parameters!(cb.condition.ma_jumps,integrator.p)
+        update_jump_params && using_params(maj) && update_parameters!(cb.condition.ma_jumps,integrator.p)
         cb.condition(cb,integrator.u,integrator.t,integrator)
     end
     nothing

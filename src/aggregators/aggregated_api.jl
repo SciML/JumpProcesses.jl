9e86107a54b4e6fe7e09cd2049efd14d2deeef24
Samuel Isaacson <isaacsas@users.noreply.github.com>
2021-07-09 16:18:49 -0400

    update doc string

--- src/aggregators/aggregated_api.jl
+++ src/aggregators/aggregated_api.jl
@@ -1,8 +1,13 @@
 """
-    reset_aggregated_jumps!(integrator,uprev = nothing)
+    reset_aggregated_jumps!(integrator,uprev = nothing; update_jump_params=true)
 
 Reset the state of jump processes and associated solvers following a change
 in parameters or such.
+
+Notes
+    - `update_jump_params=true` will recalculate the rates stored within any 
+      MassActionJump that was built from the parameter vector. If the parameter
+      vector is unchanged this can safely be set to false to improve performance.
 """
 
 function reset_aggregated_jumps!(integrator,uprev = nothing; update_jump_params=true)

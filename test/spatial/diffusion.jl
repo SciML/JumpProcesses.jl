28e52155899432e6556d22888c29edccf9b2a00a
Vasily Ilin <vasilin97@gmail.com>
2021-07-10 09:09:52 -0700

    fixed the 2D diffusion test!

--- test/spatial/diffusion.jl
+++ test/spatial/diffusion.jl
@@ -12,7 +12,7 @@ function get_mean_sol(jump_prob, Nsims, saveat)
 end
 
 # assume sites are labeled from 1 to num_sites(spatial_system)
-function discrete_laplacian_from_spatial_system(spatial_system)
+function discrete_laplacian_from_spatial_system(spatial_system, hopping_rate)
     sites = 1:DiffEqJump.num_sites(spatial_system)
     laplacian = zeros(Int, length(sites), length(sites))
     for site in sites
@@ -21,7 +21,7 @@ function discrete_laplacian_from_spatial_system(spatial_system)
             laplacian[site, nb] = 1
         end
     end
-    laplacian
+    laplacian*hopping_rate # is this part of laplacian?
 end
 
 reactstoch = []
@@ -34,7 +34,7 @@ u0 = [100]
 domain_size = 1.0 #μ-meter
 linear_size = 5
 diffusivity = 0.1
-dim = 1
+dim = 2
 grid = LightGraphs.grid([linear_size for i in 1:dim])
 num_nodes = DiffEqJump.num_sites(grid)
 
@@ -51,33 +51,31 @@ alg = NSM()
 spatial_jump_prob = JumpProblem(prob, alg, majumps, hopping_constants=hopping_constants, spatial_system=grid, save_positions=(false,false))
 
 num_time_points = 10
-Nsims = 1000
+Nsims = 10000
 solution = solve(spatial_jump_prob, SSAStepper(), saveat = tf/num_time_points).u
 mean_sol = get_mean_sol(spatial_jump_prob, Nsims, tf/num_time_points)
 
-# analytic solution THIS IS WRONG
-lap = discrete_laplacian_from_spatial_system(grid)
-h = domain_size/linear_size
-evals, Q = eigen(lap) # lap == Q*diagm(evals)*Q'
-Qt = Q'
-analytic_solution(t) = Q*diagm(ℯ.^(diffusivity*t*evals/h^2))*Qt * reshape(prob.u0, num_nodes, 1)
+# analytic solution
+lap = discrete_laplacian_from_spatial_system(grid, hopping_rate)
+evals, B = eigen(lap) # lap == B*diagm(evals)*B'
+Bt = B'
+analytic_solution(t) = B*diagm(ℯ.^(t*evals))*Bt * reshape(prob.u0, num_nodes, 1)
 
 # test mean solution
-n = num_nodes
-h = domain_size/linear_size
-D = diffusivity
-eval_lap(j, h, n) = -4/h^2 * (sin(pi*(j-1)/(2n)))^2
-evec_lap(j,n) = j==1 ? [√(1/n) for i in 1:n] : [√(2/n) * cos(pi*(j-1)*(i-0.5)/n) for i in 1:n]
-Q = hcat([evec_lap(j,n) for j in 1:n]...)
-Qt = Q'
-lambdas = [eval_lap(j, h, n) for j in 1:n]
-analytic_solution(t) = Q*diagm(ℯ.^(D*t*lambdas))*Qt * reshape(prob.u0, num_nodes, 1)
+# n = num_nodes
+# D = diffusivity
+# h = domain_size/linear_size
+# eval_lap(j, h, n) = -4/h^2 * (sin(pi*(j-1)/(2n)))^2
+# evec_lap(j,n) = j==1 ? [√(1/n) for i in 1:n] : [√(2/n) * cos(pi*(j-1)*(i-0.5)/n) for i in 1:n]
+# Q = hcat([evec_lap(j,n) for j in 1:n]...)
+# Qt = Q'
+# lambdas = [eval_lap(j, h, n) for j in 1:n]
+# analytic_solution2(t) = Q*diagm(ℯ.^(D*t*lambdas))*Qt * reshape(prob.u0, num_nodes, 1)
 
 rel_tol = 0.01
 
 times = 0.0:tf/num_time_points:tf
 for (i,t) in enumerate(times)
     local diff = analytic_solution(t) - reshape(mean_sol[i], num_nodes, 1)
-    @show diff
     @test abs(sum(diff[1:center_node])/sum(analytic_solution(t)[1:center_node])) < rel_tol
 end

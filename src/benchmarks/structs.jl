6815837e9b256b81c85c12c03e1061b23a9238dd
Vasily Ilin <vasilin97@gmail.com>
2021-08-16 13:51:47 -0700

    finished script and plotting

--- src/benchmarks/structs.jl
+++ src/benchmarks/structs.jl
@@ -19,15 +19,16 @@ minexponent = exponent(minrate)
 minrate = 2.0^minexponent
 ratetogroup = rate -> DiffEqJump.priortogid(rate, minexponent)
 
-setup_nums = [2^k for k in 6:16]
-update_nums = [2^k for k in 6:20]
+setup_nums = [2^k for k in 6:15]
+update_nums = [2^k for k in 6:25]
 pq_setups = Vector{BenchmarkTools.Trial}(undef, length(setup_nums))
 pt_setups = Vector{BenchmarkTools.Trial}(undef, length(setup_nums))
 
-pq_updates = Vector{BenchmarkTools.Trial}(undef, length(update_nums))
-pt_updates = Vector{BenchmarkTools.Trial}(undef, length(update_nums))
+pq_updates = Vector{Float64}(undef, length(update_nums))
+pt_updates = Vector{Float64}(undef, length(update_nums))
 
-seconds = 5
+seconds = 3
+samples = 10^5
 
 for i in 1:length(setup_nums)
     global num_rates = setup_nums[i]
@@ -35,7 +36,7 @@ for i in 1:length(setup_nums)
 
     # PT setup
     println("benchmark PT setup")
-    global rates = rand(num_rates)
+    rates = rand(num_rates)
     pt = DiffEqJump.PriorityTable(ratetogroup, zeros(1), minrate, 2*minrate)
     pt_setup!(pt, rates)
     b = @benchmarkable pt_setup!($pt, rates) setup = (rates = rand(num_rates)) seconds = seconds
@@ -53,39 +54,47 @@ for i in 1:length(update_nums)
 
     # PT updating
     println("benchmark PT update")
-    global rates = rand(num_rates)
+    rates = rand(num_rates)
     pt = DiffEqJump.PriorityTable(ratetogroup, zeros(1), minrate, 2*minrate)
     pt_setup!(pt, rates)
-    b = @benchmarkable DiffEqJump.update!($pt, n, oldrate, rates[n]) setup = (n = rand(1:num_rates); oldrate = rates[n]; rates[n] = rand()) seconds = seconds
-    pt_updates[i] = run(b)
+    DiffEqJump.update!(pt, 1, rates[1], rates[1]) #force compilation
+    elapsed_time = 0.0
+    for j in 1:samples
+        n = rand(1:num_rates); oldrate = rates[n]; rates[n] = rand()
+        elapsed_time += @elapsed DiffEqJump.update!(pt, n, oldrate, rates[n])
+    end
+    pt_updates[i] = elapsed_time/samples
 
     # PQ update
     println("benchmark PQ update")
-    global rates = rand(num_rates)
+    rates = rand(num_rates)
     pq = pq_setup(rates)
-    b = @benchmarkable update!($pq, n, randexp() / rates[n]) setup = (n = rand(1:num_rates); rates[n] = rand()) seconds = seconds
-    pq_updates[i] = run(b)
+    elapsed_time = 0.0
+    update!(pq, 1, randexp() / rates[1]) #force compilation
+    for j in 1:samples
+        n = rand(1:num_rates); rates[n] = rand()
+        elapsed_time += @elapsed update!(pq, n, randexp() / rates[n]) 
+    end
+    pq_updates[i] = elapsed_time/samples
 end
 
-using JLD
-save("benchmark_data/structs_bench.jld", "setup_nums", setup_nums, "update_nums", update_nums, "pq_setups", pq_setups, "pt_setups", pt_setups, "pq_updates", pq_updates, "pt_updates", pt_updates)
-
-using Plots
+using Plots, LaTeXStrings
 get_median_times(trials) = [median(trial).time for trial in trials]
-plt_setups = plot(setup_nums, get_median_times(pq_setups)./1000, label = "PQ", marker = :hex)
-plot!(plt_setups, setup_nums, get_median_times(pt_setups)./1000, label = "PT", marker = :hex, size = (800, 500))
+plt_setups = plot(setup_nums, get_median_times(pq_setups)./1000, label = "PQ", marker = :hex, size = (800, 500))
+plot!(plt_setups, setup_nums, get_median_times(pt_setups)./1000, label = "PT", marker = :hex)
 plot!(plt_setups, xaxis = :log, yaxis = :log)
 xticks!(plt_setups, setup_nums)
 ylabel!(plt_setups, "median time in microseconds, log scale")
 xlabel!(plt_setups, "number of propensities, log scale")
 title!(plt_setups, "Setup time")
 
-plt_updates = plot(update_nums, get_median_times(pq_updates), label = "PQ", marker = :hex)
-plot!(plt_updates, update_nums, get_median_times(pt_updates), label = "PT", marker = :hex, size = (800, 500))
-plot!(plt_setups, xaxis = :log)
-ylabel!(plt_updates, "median time in nanoseconds")
+plt_updates = plot(update_nums, pq_updates*1e9, label = "PQ", marker = :hex, size = (800, 500))
+plot!(plt_updates, update_nums, pt_updates*1e9, label = "PT", marker = :hex)
+ylabel!(plt_updates, "average time in nanoseconds")
 xlabel!(plt_updates, "number of propensities, log scale")
 title!(plt_updates, "Update time")
-xticks!(plt_updates, update_nums)
+# xticks!(plt_updates, (update_nums, string.(update_nums)))
+xticks = [latexstring("2^{$(Int(log(2,num)))}") for num in update_nums]
+plot!(plt_updates, xaxis = :log, xticks = (update_nums, xticks))
 
-plot(plt_setups, plt_updates)
+# plot(plt_setups, plt_updates)

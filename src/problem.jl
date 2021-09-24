f82fe625bb675d0fe5da1cfdc91eb678809ed748
Yingbo Ma <mayingbo5@gmail.com>
2021-09-24 09:36:45 -0400

    Respect u0 type

--- src/problem.jl
+++ src/problem.jl
@@ -126,7 +126,8 @@ function extend_problem(prob::DiffEqBase.AbstractODEProblem,jumps)
     prob.f(du.u,u.u,p,t)
     update_jumps!(du,u,p,t,length(u.u),jumps.variable_jumps...)
   end
-  u0 = ExtendedJumpArray(prob.u0,[-randexp() for i in 1:length(jumps.variable_jumps)])
+  T = eltype(prob.u0)
+  u0 = ExtendedJumpArray(prob.u0,[T(-randexp()) for i in 1:length(jumps.variable_jumps)])
   remake(prob,f=ODEFunction{true}(jump_f),u0=u0)
 end
 
@@ -146,7 +147,8 @@ function extend_problem(prob::DiffEqBase.AbstractSDEProblem,jumps)
     end
   end
 
-  u0 = ExtendedJumpArray(prob.u0,[-randexp() for i in 1:length(jumps.variable_jumps)])
+  T = eltype(prob.u0)
+  u0 = ExtendedJumpArray(prob.u0,[T(-randexp()) for i in 1:length(jumps.variable_jumps)])
   remake(prob,f=SDEFunction{true}(jump_f,jump_g),g=jump_g,u0=u0)
 end
 
@@ -155,7 +157,8 @@ function extend_problem(prob::DiffEqBase.AbstractDDEProblem,jumps)
     prob.f(du.u,u.u,h,p,t)
     update_jumps!(du,u,p,t,length(u.u),jumps.variable_jumps...)
   end
-  u0 = ExtendedJumpArray(prob.u0,[-randexp() for i in 1:length(jumps.variable_jumps)])
+  T = eltype(prob.u0)
+  u0 = ExtendedJumpArray(prob.u0,[T(-randexp()) for i in 1:length(jumps.variable_jumps)])
   ramake(prob,f=DDEFunction{true}(jump_f),u0=u0)
 end
 
@@ -165,7 +168,8 @@ function extend_problem(prob::DiffEqBase.AbstractDAEProblem,jumps)
     prob.f(out.u,du.u,u.u,t)
     update_jumps!(du,u,t,length(u.u),jumps.variable_jumps...)
   end
-  u0 = ExtendedJumpArray(prob.u0,[-randexp() for i in 1:length(jumps.variable_jumps)])
+  T = eltype(prob.u0)
+  u0 = ExtendedJumpArray(prob.u0,[T(-randexp()) for i in 1:length(jumps.variable_jumps)])
   remake(prob,f=DAEFunction{true}(jump_f),u0=u0)
 end
 

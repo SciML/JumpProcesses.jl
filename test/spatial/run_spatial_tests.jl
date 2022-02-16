d3f92c99984608a9b95795b03ffa25f2efa65b8d
Vasily Ilin <vasilin97@gmail.com>
2022-02-15 16:15:38 -0800

    small fix

--- test/spatial/run_spatial_tests.jl
+++ test/spatial/run_spatial_tests.jl
@@ -4,5 +4,5 @@ using DiffEqJump, DiffEqBase, SafeTestsets
   @time @safetestset "Spatial utilities" begin include("utils_test.jl") end
   @time @safetestset "Spatial A + B <--> C" begin include("ABC.jl") end
   @time @safetestset "Pure diffusion" begin include("diffusion.jl") end
-  @time @safetestset "Spatially Varying Reaction Rates" begin include("spatial/spatial_majump.jl") end
+  @time @safetestset "Spatially Varying Reaction Rates" begin include("spatial_majump.jl") end
 end

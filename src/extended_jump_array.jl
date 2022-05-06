7503c79d8e9cc2464ff7b441a2ac7e127a091b11
Sam Isaacson <isaacsas@users.noreply.github.com>
2022-05-06 14:27:42 -0400

    update ExtendedJumpArray docs

--- src/extended_jump_array.jl
+++ src/extended_jump_array.jl
@@ -2,30 +2,60 @@
 $(TYPEDEF)
 
 Extended state definition used within integrators when there are
-`VariableRateJump`s in a system. 
+`VariableRateJump`s in a system. For detailed examples and usage information see
+the
+- [Tutorial](https://diffeq.sciml.ai/stable/tutorials/discrete_stochastic_example/)
 
-## Notes
-- `VariableRateJump` `affect!` functions that wish to modify the state
-  should access `integrator.u.u` instead of the usual `integrator.u`. 
+### Fields
+
+$(FIELDS)
 
 ## Examples
 ```julia
 using DiffEqJump, OrdinaryDiffEq
 f(du,u,p,t) = du .= 0
 rate(u,p,t) = (1+t)*u[1]*u[2]
-affect!(integrator) = integrator.u.u .-= 1
-u0 = [10.0, 10.0]
 
+# suppose we wish to decrease each of the two variables by one
+# when a jump occurs
+function affect!(integrator) 
+   # Method 1, direct indexing works like normal
+   integrator.u[1] -= 1
+   integrator.u[2] -= 1
+
+   # Method 2, if we want to broadcast or use array operations we need
+   # to access integrator.u.u which is the actual state object. 
+   # So equivalently to above we could have said:
+   # integrator.u.u .-= 1
+end
+
+u0 = [10.0, 10.0]
 vrj = VariableRateJump(rate, affect!)
 oprob = ODEProblem(f, u0, (0.0,2.0))
 jprob = JumpProblem(oprob, Direct(), vrj)
 sol = solve(jprob,Tsit5())
 ```
+
+## Notes
+- If `ueja isa ExtendedJumpArray` with `ueja.u` of size `N` and `ueja.jump_u` of
+  size `num_variableratejumps` then
+  ```julia
+  # for 1 <= i <= N
+  ueja[i] == ueja.u[i]       
+
+  # for N < i <= (N+num_variableratejumps)
+  ueja[i] == ueja.jump_u[i]  
+  ```
+- In a system with `VariableRateJump`s all callback, `ConstantRateJump`, and
+  `VariableRateJump` `affect!` functions will receive integrators with
+  `integrator.u` an `ExtendedJumpArray`.
+- As such, `affect!` functions that wish to modify the state via vector
+  operations should use `ueja.u.u` to obtain the aliased state object.
 """
 struct ExtendedJumpArray{T3<:Number, T1, T<:AbstractArray{T3,T1},T2} <: AbstractArray{T3,1}
   """The current state."""
   u::T
-  """The current rate/intensity/propensity value for the `VariableRateJump`s."""
+  """The current rate (i.e. hazard, intensity, or propensity) values for the `VariableRateJump`s."""
   jump_u::T2
 end
 

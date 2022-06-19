c4d86766458e296e550973b20308facd5b869c1c
Sam Isaacson <isaacsas@users.noreply.github.com>
2022-06-19 14:20:48 -0400

    fix old test for DiffEqBase change

--- test/ensemble_uniqueness.jl
+++ test/ensemble_uniqueness.jl
@@ -6,13 +6,15 @@ j1 = ConstantRateJump((u,p,t)->10,(integrator)->integrator.u[1]+=1)
 j2 = ConstantRateJump((u,p,t)->1u[1],(integrator)->integrator.u[1]-=1)
 u0 = [0]
 
-prob = DiscreteProblem(u0, (0., 100.), [])
+prob = DiscreteProblem(u0, (0., 100.))
 jump_prob = JumpProblem(prob, Direct(), j1, j2; rng=rng)
 sol = solve(EnsembleProblem(jump_prob), FunctionMap(), trajectories=3)
 @test Array(sol[1]) !== Array(sol[2])
 @test Array(sol[1]) !== Array(sol[3])
 @test Array(sol[2]) !== Array(sol[3])
+@test eltype(sol[1].u[1]) == Int
 sol = solve(EnsembleProblem(jump_prob), SSAStepper(), trajectories=3)
 @test Array(sol[1]) !== Array(sol[2])
 @test Array(sol[1]) !== Array(sol[3])
 @test Array(sol[2]) !== Array(sol[3])
+@test eltype(sol[1].u[1]) == Int

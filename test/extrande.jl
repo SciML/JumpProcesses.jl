a0bd99f2de831dd610b7450ebcb5487093aa65fd
Paul Piho <paul.piho@gmail.com>
2022-07-14 17:44:56 +0100

    check simulation with step rate function

--- test/extrande.jl
+++ test/extrande.jl
@@ -18,3 +18,13 @@ jump_prob = JumpProblem(prob,Extrande(),jump; rng=rng)
 
 integrator = init(jump_prob,Tsit5())
 sol = solve(jump_prob,Tsit5())
+
+rate2 = (u,p,t) -> t < 5.0 ? 1.0 : 0.0
+rbound2 = (u,p,t) -> 1.0 
+jump3 = VariableRateJump(rate2,affect2!,interp_points=1000;rbnd=rbound2)
+
+prob2 = ODEProblem(f,[0.0],(0.0,10.0))
+jump_prob2 = JumpProblem(prob2,Extrande(),jump3; rng=rng)
+
+sol2 = solve(jump_prob2,Tsit5())
+@test sol2(5.0)[1] == sol2[end][1]

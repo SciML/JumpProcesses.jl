c7a832c410c7df89730b8dcf355bc3ab8287ebde
gzagatti <gzagatti@users.noreply.github.com>
2022-12-07 14:08:54 +0800

    linting.

--- test/hawkes_test.jl
+++ test/hawkes_test.jl
@@ -3,9 +3,8 @@ using Test
 using StableRNGs
 rng = StableRNG(12345)
 
-
 function reset_history!(h; start_time = nothing)
-    @inbounds for i = 1:length(h)
+    @inbounds for i in 1:length(h)
         h[i] = eltype(h)[]
     end
     nothing
@@ -21,8 +20,10 @@ function hawkes_rate(i::Int, g, h)
         x = zero(typeof(t))
         for j in g[i]
             for _t in reverse(h[j])
-                λij = α*exp(-β*(t - _t))
-                if λij ≈ 0 break end
+                λij = α * exp(-β * (t - _t))
+                if λij ≈ 0
+                    break
+                end
                 x += λij
             end
         end
@@ -38,25 +39,26 @@ function hawkes_jump(i::Int, g, h)
     function L(u, p, t)
         _lrate = lrate(u, p, t)
         _urate = urate(u, p, t)
-        return _urate == _lrate ? typemax(t) : 1/(2*_urate)
+        return _urate == _lrate ? typemax(t) : 1 / (2 * _urate)
     end
     function affect!(integrator)
         push!(h[i], integrator.t)
         integrator.u[i] += 1
     end
-    return VariableRateJump(rate, affect!; lrate=lrate, urate=urate, L=L)
+    return VariableRateJump(rate, affect!; lrate = lrate, urate = urate, L = L)
 end
 
 function hawkes_jump(u, g, h)
     return [hawkes_jump(i, g, h) for i in 1:length(u)]
 end
 
-function hawkes_problem(p, agg::QueueMethod; u=[0.], tspan=(0., 50.), save_positions=(false, true),
-    g = [[1]], h = [[]])
+function hawkes_problem(p, agg::QueueMethod; u = [0.0], tspan = (0.0, 50.0),
+                        save_positions = (false, true),
+                        g = [[1]], h = [[]])
     dprob = DiscreteProblem(u, tspan, p)
     jumps = hawkes_jump(u, g, h)
     jprob = JumpProblem(dprob, agg, jumps...;
-        dep_graph=g, save_positions=save_positions, rng=rng)
+                        dep_graph = g, save_positions = save_positions, rng = rng)
     return jprob
 end
 
@@ -65,11 +67,12 @@ function f!(du, u, p, t)
     nothing
 end
 
-function hawkes_problem(p, agg; u=[0.], tspan=(0., 50.), save_positions=(false, true),
-    g = [[1]], h = [[]])
+function hawkes_problem(p, agg; u = [0.0], tspan = (0.0, 50.0),
+                        save_positions = (false, true),
+                        g = [[1]], h = [[]])
     oprob = ODEProblem(f!, u, tspan, p)
     jumps = hawkes_jump(u, g, h)
-    jprob = JumpProblem(oprob, agg, jumps...; save_positions=save_positions, rng=rng)
+    jprob = JumpProblem(oprob, agg, jumps...; save_positions = save_positions, rng = rng)
     return jprob
 end
 
@@ -83,13 +86,13 @@ function expected_stats_hawkes_problem(p, tspan)
     # J. Da Fonseca and R. Zaatour,
     # “Hawkes Process: Fast Calibration, Application to Trade Clustering and Diffusive Limit.”
     # Rochester, NY, Aug. 04, 2013. doi: 10.2139/ssrn.2294112.
-    Varλ = (Eλ*(T*κ^2 + (1-κ^2)*(1-exp(-T*γ))/γ))/(T^2)
+    Varλ = (Eλ * (T * κ^2 + (1 - κ^2) * (1 - exp(-T * γ)) / γ)) / (T^2)
     return Eλ, Varλ
 end
 
-u0 = [0.]
+u0 = [0.0]
 p = (0.5, 0.5, 2.0)
-tspan = (0., 200.)
+tspan = (0.0, 200.0)
 g = [[1]]
 h = [Float64[]]
 
@@ -99,7 +102,7 @@ algs = (Direct(), QueueMethod())
 Nsims = 250
 
 for alg in algs
-    jump_prob = hawkes_problem(p, alg; u=u0, tspan=tspan, g=g, h=h)
+    jump_prob = hawkes_problem(p, alg; u = u0, tspan = tspan, g = g, h = h)
     if typeof(alg) <: QueueMethod
         stepper = SSAStepper()
     else
@@ -116,7 +119,6 @@ for alg in algs
         cols = length(sols[1].u[1].u)
         λs = permutedims(mapreduce((sol) -> empirical_rate(sol), hcat, sols))[:, 1:cols]
     end
-    @test isapprox(mean(λs), Eλ; atol=0.01)
-    @test isapprox(var(λs), Varλ; atol=0.001)
+    @test isapprox(mean(λs), Eλ; atol = 0.01)
+    @test isapprox(var(λs), Varλ; atol = 0.001)
 end
-

640eced1531d3c52cdb753f8eeb448b62be6decc
Sam Isaacson <isaacsas@users.noreply.github.com>
2023-01-05 12:01:49 -0500

    fix spatial jump check

--- src/problem.jl
+++ src/problem.jl
@@ -184,11 +184,12 @@ function JumpProblem(prob, aggregator::AbstractAggregatorAlgorithm, jumps::JumpS
     end
 
     ## Spatial jumps handling
-    if spatial_system !== nothing && hopping_constants !== nothing &&
-       !is_spatial(aggregator)
+    if spatial_system !== nothing && hopping_constants !== nothing
         (num_crjs(jumps) == num_vrjs(jumps) == 0) ||
             error("Spatial aggregators only support MassActionJumps currently.")
-        prob, maj = flatten(maj, prob, spatial_system, hopping_constants; kwargs...)
+        if !is_spatial(aggregator)
+            prob, maj = flatten(maj, prob, spatial_system, hopping_constants; kwargs...)
+        end
     end
 
     if is_spatial(aggregator)

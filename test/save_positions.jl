a8ec135864863f05c2ca5c5bcb1b0d92e2f949fb
gzagatti <gzagatti@users.noreply.github.com>
2023-07-14 22:20:29 +0800

    adds comment

--- test/save_positions.jl
+++ test/save_positions.jl
@@ -8,6 +8,9 @@ for alg in (Coevolve(), )
     tspan = (0.0, 30.0)
 
     dprob = DiscreteProblem(u0, tspan)
+    # set the rate to 0, so that no jump ever occurs; but urate is positive so
+    # Coevolve will consider many candidates before the end of the simmulation.
+    # None of these points should be saved.
     jump = VariableRateJump((u, p, t) -> 0, (integrator) -> integrator.u[1] += 1;
                             urate = (u, p, t) -> 1.0, rateinterval = (u, p, t) -> 5.0)
     jumpproblem = JumpProblem(dprob, alg, jump; dep_graph = [[1]],

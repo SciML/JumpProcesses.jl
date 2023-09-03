aa9edad238b66a1f960795736f2f3cf6a4259bfe
Vasily Ilin <vasilin97@gmail.com>
2023-09-02 22:28:14 -0400

    Create `SpatialConstantRateJump`.

---
+++ src/spatial/spatial_constant_rate_jump.jl
@@ -0,0 +1,6 @@
+struct SpatialConstantRateJump{F1, F2} <: AbstractJump
+    """Function `rate(u,p,t)` that returns the jump's current rate."""
+    rate::F1
+    """Function `affect(integrator)` that updates the state for one occurrence of the jump."""
+    affect!::F2
+end

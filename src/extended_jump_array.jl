92d45b7d7c3b58449ce50f88856c47fc43513664
Sam Isaacson <isaacsas@users.noreply.github.com>
2023-12-29 19:34:52 -0500

    typo 2

--- src/extended_jump_array.jl
+++ src/extended_jump_array.jl
@@ -157,7 +157,7 @@ end
 function Base.Broadcast.BroadcastStyle(::S, ::Base.Broadcast.Unknown) where {
         UStyle, JumpUStyle, S<:JumpProcesses.ExtendedJumpArrayStyle{UStyle, JumpUStyle}}
 
-    return throw(ArgumentError("Cannot broadcast JumpProcesse.ExtendedJumpArray with" *
+    return throw(ArgumentError("Cannot broadcast JumpProcesses.ExtendedJumpArray with" *
                                " something of type Base.Broadcast.Unknown."),)
 end
 

36ec0c11cca1f9d16d21fc448a4b2e67259e023d
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2025-02-21 16:18:40 +0530

    added callable cache
    Signed-off-by: sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>

--- test/variable_rate.jl
+++ test/variable_rate.jl
@@ -22,7 +22,15 @@ a .= b .+ c .+ d
 
 rate = (u, p, t) -> u[1]
 affect! = (integrator) -> (integrator.u[1] = integrator.u[1] / 2)
+
+
+f = function (du, u, p, t)
+    du[1] = u[1]
+end
+prob = ODEProblem(f, [0.2], (0.0, 10.0))
 jump = VariableRateJump(rate, affect!, interp_points = 1000)
+JumpSet(jump).variable_jumps[1]
+
 jump2 = deepcopy(jump)
 
 f = function (du, u, p, t)

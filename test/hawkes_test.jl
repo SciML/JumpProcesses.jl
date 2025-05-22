3236a26d3d4a961fe6ca23a6751334a2b38dd893
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2025-05-23 01:43:10 +0530

    cleaned a test

--- test/hawkes_test.jl
+++ test/hawkes_test.jl
@@ -77,7 +77,7 @@ function hawkes_problem(p, agg; u = [0.0], tspan = (0.0, 50.0),
         save_positions = (false, true), g = [[1]], h = [[]], uselrate = true, vr_aggregator = VR_FRM(), kwargs...)
     oprob = ODEProblem(f!, u, tspan, p)
     jumps = hawkes_jump(u, g, h)
-    jprob = JumpProblem(oprob, agg, jumps...; vr_aggregator; save_positions, rng)
+    jprob = JumpProblem(oprob, agg, jumps...; vr_aggregator, save_positions, rng)
     return jprob
 end
 

9497b89bb886f317e7e1ba62466b7d9cfece42fd
Documenter.jl <documenter@juliadocs.github.io>
2025-09-09 09:56:28 +0000

    build based on acb5052

--- versions.js
+++ versions.js
@@ -1,8 +1,9 @@
 var DOC_VERSIONS = [
   "stable",
+  "v9.18",
   "v9.17",
   "v9.16",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v9.17.0";
+var DOCUMENTER_NEWEST = "v9.18.0";
 var DOCUMENTER_STABLE = "stable";

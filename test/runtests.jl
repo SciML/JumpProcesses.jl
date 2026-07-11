f0e4c3afdacfd0960caa13b16d1bd9d0e5817739
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-11 07:48:30 -0400

    Use shared rendered API docs QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,7 +12,6 @@ end
 @time begin
     if GROUP == "QA"
         @time @safetestset "QA Tests" begin include("qa.jl") end
-        @time @safetestset "Public API Documentation" begin include("public_api_docs.jl") end
     end
     
     if GROUP == "All" || GROUP == "InterfaceI"
